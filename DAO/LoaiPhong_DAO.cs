5cfcb9a69318c05a41f3f9e97f0600554006097c
Minh Sĩ <TranMinhSi005@gmail.com>
2026-05-07 22:27:41 +0700

    code cho nút thêm sửa xóa loại phòng, kiểm tra các control hợp lệ, viết thêm hàm insert, update, delete cho quản lý loại phòng

--- DAO/LoaiPhong_DAO.cs
+++ DAO/LoaiPhong_DAO.cs
@@ -18,8 +18,63 @@ namespace QuanLyKhachSan.DAO
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Lỗi: " + ex.Message);
+                MessageBox.Show("Lỗi: " + ex.Message);
                 return new List<LoaiPhong_DTO>();
+            }
+        }
+        public bool InsertLoaiPhong(LoaiPhong_DTO loaiPhong)
+        {
+            try
+            {
+                db.LoaiPhong_Entities.Add(loaiPhong);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+                return false;
+            }
+        }
+        public bool UpdateLoaiPhong(LoaiPhong_DTO loaiPhong)
+        {
+            try
+            {
+                var phong = db.LoaiPhong_Entities.Find(loaiPhong.MaLoaiPhong);
+                if (phong != null)
+                {
+                    phong.TenLoaiPhong = loaiPhong.TenLoaiPhong;
+                    phong.GiaTheoNgay = loaiPhong.GiaTheoNgay;
+                    phong.SoNguoiToiDa = loaiPhong.SoNguoiToiDa;
+                    phong.MoTa = loaiPhong.MoTa;
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+                return false;
+            }
+        }
+        public bool DeleteLoaiPhong(int maLoaiPhong)
+        {
+            try
+            {
+                var phong = db.LoaiPhong_Entities.Find(maLoaiPhong);
+                if (phong != null)
+                {
+                    db.LoaiPhong_Entities.Remove(phong);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+                return false;
             }
         }
     }
